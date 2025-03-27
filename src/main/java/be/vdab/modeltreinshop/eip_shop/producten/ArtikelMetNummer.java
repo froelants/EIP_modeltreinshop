163a23d6fc7653bb1be02337f37dd0eaa4d5c84a
frank.roelants <frank.roelants@gmail.com>
2025-03-27 20:29:17 +0100

    Class ArtikelMetNummer,fouten in de test ArtikelMetNummerTest nog uit te zoeken

--- src/main/java/be/vdab/modeltreinshop/eip_shop/producten/ArtikelMetNummer.java
+++ src/main/java/be/vdab/modeltreinshop/eip_shop/producten/ArtikelMetNummer.java
@@ -1,7 +1,6 @@
 package be.vdab.modeltreinshop.eip_shop.producten;
 
 import java.io.Serial;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/test/java/be/vdab/modeltreinshop/eip_shop/producten/ArtikelMetNummerTest.java
+++ src/test/java/be/vdab/modeltreinshop/eip_shop/producten/ArtikelMetNummerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
