87fa9c1641e54192146c72a48054b93a29d638c1
frank.roelants <frank.roelants@gmail.com>
2025-03-27 11:11:54 +0100

    Gegenereerd project met spring.io

---
+++ src/main/java/be/vdab/modeltreinshop/eip_shop/EipShopApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.modeltreinshop.eip_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EipShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EipShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/vdab/modeltreinshop/eip_shop/EipShopApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.modeltreinshop.eip_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EipShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
